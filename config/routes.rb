155428bda9c2258c47422a1df0ac7678560fd723
watakenoko <b1016127@fun.ac.jp>
2019-07-16 11:51:19 +0900

    home and help page change (3.2)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'static/Pages'
+  get 'static_pages/home'
 
-  get 'static/homme'
-
-  get 'static/help'
+  get 'static_pages/help'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "application#hello"
 end
